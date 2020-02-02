a9da2a583e3a71138407d01cf86cf52fb8645ac6
Samuel Odundo <samieodd@gmail.com>
2020-02-02 17:55:10 +0300

    Converted example into a constant

--- logic/get_sum.cpp
+++ logic/get_sum.cpp
@@ -5,10 +5,13 @@ std::string get_sum()
 {
   // variable to store the sum
   std::string full_sum;
+  std::string q_format = "1 + 1";
 
   // ask for sum
   std::cout << "Please type in your sum:\n";
-  std::cout << "(e.g '1 + 1')\n";
+  std::cout << "(e.g "
+            << "'" << q_format << "'"
+            << ")\n";
   std::getline(std::cin, full_sum); // using getline() to capture the whole input and not ignore spaces
 
   // return the given sum
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <stdlib.h>
+#include <ctype.h>
 
 // import code stored in other files
 #include "logic/get_sum.cpp"
@@ -15,6 +16,7 @@ int main()
   // declare variables to be used
   double first_number, second_number, answer;
   std::string question_operator, full_sum;
+  std::string question_format = "1 + 1";
 
   // use function to request the
   // question from the user and
@@ -26,6 +28,7 @@ int main()
   std::vector<std::string> split_string = split_sum(full_sum);
 
   std::string sum_parts[3];
+
   if (split_string.size() > 3)
   {
     std::cout << "ERROR: Please enter only two numbers!";
@@ -40,10 +43,9 @@ int main()
     std::copy(split_string.begin(), split_string.end(), sum_parts);
   }
 
-  // assign the parts of the sum to
-  // different variables using the
-  // function atof with the method
-  // c_str()
+  // assign the parts of the sum to different
+  // variables using the atof function and
+  // the c_str() method
   first_number = atof(sum_parts[0].c_str());
   second_number = atof(sum_parts[2].c_str());
   question_operator = sum_parts[1]; // keep the operator as a string for reference
@@ -52,7 +54,7 @@ int main()
   answer = calculator(first_number, second_number, question_operator);
 
   // display the answer
-  std::cout << answer;
+  std::cout << "Your answer is " << answer << ".";
 
   return 0;
 }
