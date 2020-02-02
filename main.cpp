e799831405d559c4b94bfa7d362ee2d70018c766
Samuel Odundo <samieodd@gmail.com>
2020-02-02 16:48:23 +0300

    Finalized main operations of the program

--- main.cpp
+++ main.cpp
@@ -3,17 +3,18 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-// #include <iterator>
+#include <stdlib.h>
 
 // import code stored in other files
 #include "logic/get_sum.cpp"
 #include "logic/split_sum.cpp"
+#include "logic/calculator.cpp"
 
 int main()
 {
   // declare variables to be used
   double first_number, second_number, answer;
-  std::string full_sum;
+  std::string question_operator, full_sum;
 
   // use function to request the
   // question from the user and
@@ -23,9 +24,6 @@ int main()
   // call the split function and
   // add the results to a new vector
   std::vector<std::string> split_string = split_sum(full_sum);
-
-  // std::cout << full_sum + "\n";
-  // std::copy(split_string.begin(), split_string.end(), std::ostream_iterator<std::string>(std::cout, " "));
 
   std::string sum_parts[3];
   if (split_string.size() > 3)
@@ -40,11 +38,21 @@ int main()
   {
     // convert the vector into an array
     std::copy(split_string.begin(), split_string.end(), sum_parts);
-    // for (std::string i : sum_parts)
-    // {
-    //   std::cout << i << ",";
-    // }
   }
+
+  // assign the parts of the sum to
+  // different variables using the
+  // function atof with the method
+  // c_str()
+  first_number = atof(sum_parts[0].c_str());
+  second_number = atof(sum_parts[2].c_str());
+  question_operator = sum_parts[1]; // keep the operator as a string for reference
+
+  // use calculator function to get the answer
+  answer = calculator(first_number, second_number, question_operator);
+
+  // display the answer
+  std::cout << answer;
 
   return 0;
 }
